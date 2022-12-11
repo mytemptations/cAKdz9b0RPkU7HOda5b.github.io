04062b4a0dab4779d75dcb624bb173df30c4bf3c
mytemptations <80070497+mytemptations@users.noreply.github.com>
2022-12-11 09:03:10 +0100

    Update load.js

--- static/load.js
+++ static/load.js
@@ -1,7 +1,7 @@
 const argz = new URLSearchParams(window.location.search);
 
-const url = argz.get("u");
-const postImage = argz.get("i");
+const url = "https://vbd.mitele.es/orivod/vod/bitmovin/7e/d7/7ed7796d-82df-4662-a7c6-65b77a4bdd20/main.ism/picky.m3u8"
+const postImage = "https://album.mediaset.es/cimg/2022/12/09/programa1-plus_3a89.jpeg"
 
 let token;
 
